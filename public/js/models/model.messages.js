f8b845114b4541c734b83208b34b7480af9a1f1e
Enzo Martin <enzo.r.martin@gmail.com>
2013-02-04 03:05:37 +0100

    Added dismiss all

--- public/js/models/model.messages.js
+++ public/js/models/model.messages.js
@@ -14,6 +14,7 @@
 				icon: 'bullhorn'
 			},
 			from: 'Person',
+			title: 'The title is always visible',
 			message: 'A message',
 			image: 'http://i.imgur.com/dlI3AXU.jpg',
 			actions: [
@@ -30,8 +31,6 @@
 			read: false
 		},
 
-		idAttribute: "_id",
-
 		initialize: function(){
 			if(this.get('timestamp') == 0){
 				this.set('timestamp',moment().format('LLL'));
@@ -43,16 +42,31 @@
 		messages_collection: Backbone.Collection.extend({
 			model: message_model,
 
+			get_counts: function(){
+				var counts = {};
+				this.each(function(item){
+					var type = item.get('type');
+					if(_.isUndefined(counts[type.icon])){
+						counts[type.icon] = {name:type.name,count:1,icon:type.icon};
+					} else {
+						counts[type.icon].count++;
+					}
+				});
+				return counts;
+			},
+
 			make_new: function(){
 				var new_message = {};
 
 				var images = ['http://i.imgur.com/dlI3AXU.jpg','http://i.imgur.com/pmxWhPU.jpg','http://i.imgur.com/UzpofbP.jpg','http://i.imgur.com/MJ3d24T.jpg','http://i.imgur.com/8kEBPsR.jpg'];
-				var types = [{name: 'Announcement',icon: 'bullhorn'},{name: 'Meeting',icon: 'calendar'},{name: 'New Picture',icon: 'camera'},{name: 'Vehicle Destroyed',icon: 'fighter-jet'},{name: 'New Email',icon: 'envelope'}]
+				var types = [{name: 'Announcement',icon: 'bullhorn'},{name: 'Meeting',icon: 'calendar'},{name: 'Picture',icon: 'camera'},{name: 'Vehicle Destroyed',icon: 'fighter-jet'},{name: 'Email',icon: 'envelope'}];
+				var titles = ['Needs moar cowbell!','GET TO DA CHOPPA','Definitely hacks.','I totally agree somewhat possibly not really no','Bet I can fly that Viper better than you','There is no cow level','it\'s time to kickass and chew bubblegum.. and I\'m all outta gum','Schrödinger\'s cat, alive or dead? Hmmm','Most peculiar!','You must build additional pylons!'];
 				var messages = ['Needs moar cowbell!','GET TO DA CHOPPA','Definitely hacks.','I totally agree somewhat possibly not really no','Bet I can fly that Viper better than you','There is no cow level','it\'s time to kickass and chew bubblegum.. and I\'m all outta gum','Schrödinger\'s cat, alive or dead? Hmmm','Most peculiar!','You must build additional pylons!'];
 				var name = ['Leeroy','Allen','Bob','Carlton','David','Ernie','Foster','George','Howard','Ian','Jeffery','Kenneth','Lawrence','Michael','Nathen','Orson','Peter','Quinten','Reginald','Stephen','Thomas','Morris','Victor','Walter','Xavier','Charles','Anthony','Gordon','Percy','Conrad','Quincey','Armand','Jamal','Andrew','Matthew','Mark','Gerald'];
 				var actions = [[{text: 'Action 1',action: 'action-1'}],[{text: 'Action 1',action: 'action-1'},{text: 'Action 2',action: 'action-2'}],[{text: 'Action 1',action: 'action-1'},{text: 'Action 2',action: 'action-2'},{text: 'Action 3',action: 'action-3'}],[{text: 'Action 1',action: 'action-1'},{text: 'Action 2',action: 'action-2'},{text: 'Action 3',action: 'action-3'},{text: 'Action 4',action: 'action-4'},{text: 'Action 5',action: 'action-5'},{text: 'Action 6',action: 'action-6'}]];
 
 				new_message.image = images[Math.floor(Math.random()*images.length)];
+				new_message.title = titles[Math.floor(Math.random()*titles.length)];
 				new_message.message = messages[Math.floor(Math.random()*messages.length)];
 				new_message.from = name[Math.floor(Math.random()*name.length)];
 				new_message.timestamp = moment().format('LLL');
--- public/js/views/view.messages.js
+++ public/js/views/view.messages.js
@@ -2,39 +2,88 @@
 	_.extend(window.SA.prototype.view_definitions,{
 		messages_list: Backbone.View.extend({
 			events: {
-				'click .add-new-message'	: 'new_message'
+				'click .add-new-message'		: 'new_message',
+				'click .message-filter'			: 'toggle_filter',
+				'click #clear-all-messages'		: 'clear_all'
 			},
 
 			id: 'messages-list',
 
 			initialize: function(){
 				this.collection.on('add',this.add_message,this);
-				this.collection.on('destroy',this.remove_message,this);
+				this.collection.on('remove',this.remove_message,this);
 				this.message_views = {};
+				this.message_counts = MJS.get({view:{name:'message_counts'},model:this.model,collection:this.collection});
 			},
 
 			new_message: function(){
 				this.collection.make_new();
 			},
 
-			add_message: function(model){
-				console.log('message added',model);
-				var view = new MJS.view_definitions.message({model:model});
-				this.message_views[view.cid] = view;
-				this.message_views[view.cid].on('remove',function(){
-					console.log('delete',view.cid);
-					delete this.message_views[view.cid];
-				},this);
-				this.$el.find('#messages-list').prepend(view.render().el);
+			clear_all: function(e){
+				e.preventDefault();
+				var timeout = 0;
+				var views = _.toArray(this.message_views);
+				_.each(views,function(view){
+					window.setTimeout(function(){
+						view.close();
+					},timeout);
+					timeout = timeout + 100;
+				});
+			},
+
+			toggle_filter: function(e){
+				e.preventDefault();
+				var target = $(e.currentTarget);
+				var filter = target.attr('data-filter');
+				target.parent('li').toggleClass('active');
+				this.$el.find('#messages-list').toggleClass('hide-'+filter);
+				this.expand_first();
+				this.$el.find('.new-message:visible:first').removeClass('message-condensed');
+			},
+
+			expand_first: function(){
+				this.$el.find('.message-condensed:visible:first').removeClass('message-condensed');
 				this.$el.find('.new-message:not(:first)').addClass('message-condensed');
 			},
 
+			add_message: function(model){
+				var view = new MJS.view_definitions.message({model:model});
+				this.message_views[view.cid] = view;
+				this.message_views[view.cid].on('remove',function(){
+					this.collection.remove(view.model);
+					delete this.message_views[view.cid];
+				},this);
+				this.$el.find('#messages-list').prepend(view.render().el);
+				this.expand_first();
+			},
+
 			remove_message: function(model){
-				console.log('message deleted',model);
+				this.expand_first();
+				console.log('deleted');
+				this.$el.find('.message-condensed:first').removeClass('message-condensed');
 			},
 
 			render: function(){
 				this.$el.html(ich.tpl_messages_box(this.model.toJSON()));
+				this.$el.find('#messages-unread').html(this.message_counts.render().el);
+				this.delegateEvents();
+			}
+		}),
+
+		message_counts: Backbone.View.extend({
+			tagName: 'ul',
+
+			className: 'nav nav-pills',
+
+			initialize: function(){
+				this.collection.on('all',this.render,this);
+			},
+
+			render: function(){
+				var counts = _.toArray(this.collection.get_counts());
+				this.$el.html(ich.tpl_message_counts(counts));
+				return this;
 			}
 		}),
 
@@ -51,7 +100,7 @@
 			className: 'new-message',
 
 			initialize: function(){
-				console.log('new view',this);
+				//
 			},
 
 			expand: function(){
@@ -70,7 +119,9 @@
 			},
 
 			action: function(e){
-				console.log('click',e.currentTarget);
+				var target = $(e.currentTarget);
+				MJS.alert({type:'success',message:'You\'ve dismissed the notification and clicked the button ' + target.text()});
+				this.close();
 			},
 
 			render: function(){
